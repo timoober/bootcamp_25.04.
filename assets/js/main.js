2d7c3af1e73265646710ef642f593b3029dba574
timoober <timo.oberschachtsiek@gmail.com>
2023-04-25 15:19:13 +0200

    first upload

---
+++ assets/js/main.js
@@ -0,0 +1,149 @@
+// 1_1
+
+// let a = 12;
+// let b = a * 3;
+// console.log(b);
+
+// document.write(b);
+
+// console.log("Hello World!");
+
+// console.log("Anton");
+
+// let firstName = "Anton";
+// console.log({firstName});
+
+// let age = "28";
+// console.log({age});
+
+// let job = "Trainer";
+// console.log ({job});
+
+// let married = true;
+
+// console.log(firstName + "ist" + age + "Jahre alt, von Beruf" + job + "und ist verheiratet" + married);
+
+// 1_5 Variablen
+
+// let carName = "BMW"
+// console.log(carName);
+
+// let x = 150;
+// console.log(x);
+
+// let y = 50;
+// console.log(y);
+
+// let z = (x + y)
+// console.log(z);
+
+// let firstName = "John"
+// let lastName = "Doe"
+// let age = "35"
+
+// console.log(firstName + lastName + age)
+
+// 1_6 Constante
+// DAFUQ? Keine Ahnung what to do
+// {let x = 34};
+// let x = 67;
+
+// {let y = 34};
+// let y = 67;
+// console.log(y);
+
+
+// 1_12
+
+// let score = 5 + 5*10;
+// console.log('Ergebnis: ' + score);
+
+// let score = (5 + 5)*10;
+// console.log('Ergebnis: ' + score);
+
+// let score = 0;
+// score = score +10;
+// console.log('Ergebnis ' + score);
+
+// score +=10;
+// console.log('Ergebnis ' + score);
+
+// let zahl =1;
+// zahl = zahl +1;
+// zahl +=1;
+
+// console.log(zahl);
+
+// zahl++;
+// zahl--;
+// console.log('dekrement ' + zahl);
+
+
+// Lev_1_13
+
+// let addition_operator = 1 + 1;
+// console.log("addition: " + addition_operator);
+
+// let subtraktion_operator = 2-1;
+// console.log("subtraktion: " + subtraktion_operator);
+
+// let multiplication_operator = 2 * 2;
+// console.log("multiplikation " + multiplication_operator );
+
+// let division_operator = 4/2;
+// console.log("Division"  + division_operator);
+
+// let modul_operator = 14 % 5;
+// console.log("modulus" + modul_operator);
+
+// Lev_1_11
+
+// let x = "Hello ";
+// let y = "World<br>";
+// let z = x + "" + y
+// let n = z
+// // console.log(x + y);
+// document.write(x)
+
+// console.log(x + y)
+
+// let meinString = "Ich bin Erster"
+// let andererString = "Ich komme auf Platz zwei"
+// console.log(meinString, andererString);
+// console.log(meinString + " irgendwas " + andererString);
+
+// let timoTimo = meinString + andererString
+// console.log(timoTimo);
+
+// let myText = "Hello again!"
+// let vorname = "Timo"
+// let nachname = "Oberschachtsiek"
+
+// lev1_4
+// document.write("<h1>Hello World!</h1>")
+// document.write("<p>Have a nice day!</p>")
+// document.write("<h1>Hello World!</h1>")
+// document.write(myText)
+// document.write(vorname + nachname)
+
+// window.alert("Hallo Welt");
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+// window.prompt("Bitte geben Sie Ihren Namen ein,", "Cancel");
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+
+// let alter = window.prompt("Gib mir bitte dein Alter.");
+// console.log(alter);
+
+// let b=5;
+// let a= b *5-3;
+// window.alert(a)
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt :";
+// let grosse = "2,72m";
+// window.confirm(mann + grosse);
+
+
+
+
+
+
